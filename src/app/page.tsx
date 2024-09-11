ffc816ba3e8c6f4122dd1249db9c551f33ef6650
Buburry <Natapon7934@gmail.com>
2024-09-11 18:53:46 +0700

    comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,9 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z.string()
+      .min(6, { message: "Password must contain at least 6 characters" })
+      .max(12, { message: "Password must not exceed 12 characters" }),
     confirmPassword: z.string(),
   })
   .refine(
@@ -68,6 +70,18 @@ const schema = z
     {
       message: "Invalid coupon code",
       path: ["coupon"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.password !== data.confirmPassword) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
     }
   );
 
@@ -96,12 +110,17 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
     //check discount here
-
-    return price;
+    if (form.values.hasCoupon === true && form.values.coupon === "CMU2023") {
+      price =  price- (price * (30/100))
+    }
+    return price.toLocaleString();
   };
 
   return (
@@ -187,7 +206,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+         <Footer year={2024} fullName="Natapon Phairin" studentId="660610749" /> 
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
